a63a2077eae6a41062b0e452a3916db16976880f
anjara4 <anjaranobby@gmail.com>
2023-12-05 15:08:49 +0100

    Update writing_to_specific_page.c

--- writing_to_specific_page.c
+++ writing_to_specific_page.c
@@ -3,7 +3,7 @@
 int main(void)
 {
     NSS_NFC->CFG = 0;
-    NSS_NFC->BUF[0] = 12;
+    NSS_NFC->BUF[0] = 12; // This initiates 12 into the 4th page. The 4th page is the fist page for data
 
     return 0;
 }
