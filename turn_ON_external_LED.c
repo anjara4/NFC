7077fe9d969315c31bcb2f988776e3bf73809112
anjara4 <anjaranobby@gmail.com>
2023-12-06 17:22:10 +0100

    Add files via upload

---
+++ turn_ON_external_LED.c
@@ -0,0 +1,13 @@
+#include "board.h"
+
+int main(void)
+{
+   Board_Init();
+   Chip_GPIO_Init(NSS_GPIO);
+   Chip_IOCON_Init(NSS_IOCON);
+
+   Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_PIO0_2 , IOCON_FUNC_0 | IOCON_RMODE_PULLUP | IOCON_LPF_DISABLE);
+   Chip_GPIO_SetPinDIROutput(NSS_GPIO, 0, IOCON_PIO0_2);
+   Chip_GPIO_SetPinOutHigh(NSS_GPIO, 0, IOCON_PIO0_2);
+   return 0;
+}
