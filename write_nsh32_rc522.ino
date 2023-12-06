bb161088c3590d126a92acf57a14caf9953a6003
anjara4 <anjaranobby@gmail.com>
2023-12-06 17:24:53 +0100

    Update write_nsh32_rc522.ino

--- write_nsh32_rc522.ino
+++ write_nsh32_rc522.ino
@@ -1,4 +1,11 @@
-//inspired from https://devcraze.com/tutorials/arduino/mfrc522-rfid-read-and-write-data-in-specific-rfid-block/
+/*Writing data from an NFC Reader RC522 to an NHS3152 nxp chip
+This code is inspired by https://devcraze.com/tutorials/arduino/mfrc522-rfid-read-and-write-data-in-specific-rfid-block/
+The original code can be used to write data into an Mifair card but cannot work whith the NHS3152 chip
+The problem is that the authenfication for the Mifair card does not work with the NHS3152 chip
+Mifair need to be authentified in order to work
+My initial solution is to remove the authentication phase, which is not mandatory with the NHS3152 (obviously)
+The next step will be to add the authentification for the NHS3152*/
+
 #include <SPI.h>
 #include <MFRC522.h>
 #define RST_PIN         9           // Configurable, see typical pin layout above
