25973a924129f84771d2e1bd20feefbb28a10818
anjara4 <anjaranobby@gmail.com>
2023-12-07 08:26:45 +0100

    Turn OFF LED

--- turn_ON_external_LED.c
+++ turn_ON_external_LED.c
@@ -9,5 +9,6 @@ int main(void)
    Chip_IOCON_SetPinConfig(NSS_IOCON, IOCON_PIO0_2 , IOCON_FUNC_0 | IOCON_RMODE_PULLUP | IOCON_LPF_DISABLE);
    Chip_GPIO_SetPinDIROutput(NSS_GPIO, 0, IOCON_PIO0_2);
    Chip_GPIO_SetPinOutHigh(NSS_GPIO, 0, IOCON_PIO0_2);
+   //Chip_GPIO_SetPinOutLow(NSS_GPIO, 0, IOCON_PIO0_2);
    return 0;
 }
