631a09a4107f46117d67db472b3016c570c16c43
anjara4 <anjaranobby@gmail.com>
2023-12-11 08:45:08 +0100

    Update write_memory_nsh32_from_RC522.ino: code refactoring

--- write_memory_nsh32_from_RC522.ino
+++ write_memory_nsh32_from_RC522.ino
@@ -5,6 +5,11 @@ The problem is that the authenfication for the Mifair card does not work with th
 Mifair need to be authentified in order to work
 My initial solution is to remove the authentication phase, which is not mandatory with the NHS3152 (obviously)
 The next step will be to add the authentification for the NHS3152*/
+/*Integration of the DumpInfo.ino program into this code. 
+  In this way, writing and reading the information inside the shared memory is possible with the same program*/ 
+/*The original code does not allow read and write mutliple time.
+By removing the PICC_HaltA() inside the code for writing 
+and also inside the ...\Arduino\libraries\MFRC522\src\MFRC522.c for the reading*/
 
 #include <SPI.h>
 #include <MFRC522.h>
@@ -24,25 +29,24 @@ void loop() {
   if ( ! mfrc522.PICC_IsNewCardPresent()) {return;}
   if ( ! mfrc522.PICC_ReadCardSerial()) {return;}
   Serial.setTimeout(20000L);
-  writingNFC();
-}
-
-void writingNFC(){
-  byte pageNumber = 4;
+  
   byte dataValue = 1;
-
   if (Serial.available() > 0) {
     dataValue = Serial.parseInt();
-    
+
     if(dataValue > 0){
-      if(dataValue == 9) {
-        mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-      }
-      Serial.print("I received: ");
-      Serial.println(dataValue);
-      byte buffr[] = {dataValue,0x0,0x0,0x0};
-      mfrc522.MIFARE_Write(pageNumber, buffr, 16); 
+      if(dataValue == 9) //Print data from the shared memory
+        mfrc522.PICC_DumpToSerial(&(mfrc522.uid)); //comment the PICC_HaltA() from ...\Arduino\libraries\MFRC522\src\MFRC522.c
+      
+      writingNFC(4, dataValue);//write datavalue into the page 4 of the shared memory
     }
   }
+}
+
+void writingNFC(byte pageNumber, byte dataValue){
+  Serial.print("I received: ");
+  Serial.println(dataValue);
+  byte buffr[] = {dataValue,0x0,0x0,0x0};
+  mfrc522.MIFARE_Write(pageNumber, buffr, 16); 
 }
 
